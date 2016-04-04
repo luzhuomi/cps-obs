5107d78348a6d9e3f45c528aeba6a940c861826d
Kenny Lu <luzhuomi@gmail.com>
2016-04-04 11:17:56 +0800

    added test cases

---
+++ test/Test.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE FlexibleContexts #-}
+import System.Environment(getArgs)
+import Control.Monad.IO.Class(liftIO)
+import Clang.String(unpack)
+import Clang.TranslationUnit(getDiagnosticSet)
+import Clang(parseSourceFile)
+import qualified Clang.Diagnostic as Diagnostic(format, getElements)
+
+test tu = 
+  do getDiagnosticSet tu >>= Diagnostic.getElements >>= mapM_ printDiag
+    where printDiag d = Diagnostic.format Nothing d >>=
+                        unpack >>=
+                        (liftIO . putStrLn . ("Diag:" ++))
+
+main = do
+  (arg:args) <- getArgs
+  parseSourceFile arg args test
