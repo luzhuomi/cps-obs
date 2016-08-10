b9f0c6175c2ff2306d3fecbe75a0406b8e538f8d
Kenny Lu <luzhuomi@gmail.com>
2016-08-10 15:19:58 +0800

    updated

--- test/Test.hs
+++ test/Test.hs
@@ -2,10 +2,10 @@
 import System.Environment(getArgs)
 import Control.Monad.IO.Class(liftIO, MonadIO)
 import Clang.String(unpack)
-import Clang.TranslationUnit(getDiagnosticSet, getSpelling, getCursor)
+import Clang.TranslationUnit(getDiagnosticSet, {-getSpelling,-} getCursor)
 import Clang(parseSourceFile)
 import Clang
-import Clang.Cursor hiding (getSpelling)
+import Clang.Cursor 
 import qualified Clang.Diagnostic as Diagnostic(format, getElements)
 import Control.Monad.Trans.Control(MonadBaseControl)
 import Control.Monad.Catch(MonadThrow)
@@ -22,22 +22,57 @@ test tu =
     where printDiag d = Diagnostic.format Nothing d >>=
                         unpack >>=
                         (liftIO . putStrLn . ("Diag:" ++))
-
+{-
 test2 tu = do 
   { clangString <- getSpelling tu
   ; string <- unpack clangString
   ; (liftIO $ putStrLn string)
   }
-           
+-}         
+  
+
 test3 tu = do 
   { cursor <- getCursor tu
-  ; printKind cursor
-  ; cursors  <- getChildren cursor
-  ; DVS.mapM_ printCursorInfo cursors
-  ; return ()
+  ; mains <- getMain cursor
+  ; if null mains
+    then return ()
+    else do 
+      { let main = head mains
+      ; traverseCursor main
+      }
   }
-    
-printCursorInfo cursor = liftIO $ putStrLn (show (getKind cursor))
+           
+getMain cursor = do 
+  { displayName <- getDisplayName cursor
+  ; unpackedName <- unpack displayName
+  ; if (unpackedName == "main()") 
+    then return [cursor]
+    else do 
+      { cursors <- getChildren cursor
+      ; rs <- mapM getMain (DVS.toList cursors)
+      ; return (concat rs) 
+      }
+  }
+
+
+traverseCursor cursor = do 
+  { printCursorInfo cursor
+  ; cursors <- getChildren cursor
+  ; DVS.mapM_ traverseCursor cursors
+  }
+                   
+        
+
+
+printCursorInfo cursor = do 
+  { displayName <- getDisplayName cursor 
+  ; unpackedName <- unpack displayName
+  ; spelling <- getSpelling cursor  
+  ; unpackedSpelling <- unpack spelling
+  ; usr <- getUSR cursor
+  ; unpackedUSR <- unpack usr
+  ; liftIO $ putStrLn ( (show (getKind cursor)) ++ "\t" ++ unpackedName ++ "\t" ++ unpackedSpelling ++ "\t" ++ unpackedUSR )
+  }
 
 
 main = do
