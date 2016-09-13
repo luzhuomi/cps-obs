8d839b52f6fa6b5cc1a6bdcb22b0dffc68d9a91c
Kenny Lu <luzhuomi@gmail.com>
2016-09-13 17:16:59 +0800

    updated

--- Language/C/Obfuscate/CFG.hs
+++ Language/C/Obfuscate/CFG.hs
@@ -25,8 +25,43 @@ stmts ::= stmt | stmt; stmts
 stmt ::= x = stmt | if (exp) { stmts } else {stmts} | 
          return exp | exp | while (exp) { stmts } 
 exp ::= exp op exp | (exp) | id | exp op | op exp | id (exp, ... exp) 
+-}
 
+
+{-
+x \not\in lhs(stmts')
+{i}, S, N \cup { i->stmts' ++ {x=exp} } |- stmts : I, S', N'
+------------------------------------------------------------- (Assign1)
+{i}, S, N\cup{i->stmts'} |- x=exp; stmts : I, S', N'
+-}
+
+{-
+x \in lhs(stmts')  
+j is fresh
+{j}, S \pcup { i-> {j} }, N \cup { i->stmts', j->{x=exp}} |- stmst: I, S',N' 
+-----------------------------------------------------------------------------(Assign2)
+{i}, S, N\cup{i->stmts'} |- x=exp; stmts : I, S', N'
+
+where
+S \pcup { i->{j} } := S\i \cup { i -> S(i) \cup {j} } 
+-}
+       
+
+{-
+j, k, l are fresh
+S' = S \cup{i->{j,k}}
+N' = N \cup { i->stmts' \cup { if (cond) {stmts1} else {stmts2} } }
+{j}, S', N' \cup {j->{}} |- stmts1, I1, S1, N1
+{k}, S', N' \cup {k->{}} |- stmts2, I2, S2, N2
+{l}, S1 \cup S2 \cup { n -> {m} | n \in I1\cup I2 }, N1 \cup N2 \cup {m->{}} |- stmts: I',S',N'
+-------------------------------------------------------------------------------------------------- (If)
+I,S,N\cup { i->stmts'} |- if (cond) { stmts1 } else { stmts2 }; stmts: I',S',N'
+-}
+
+{-
+j, k are fresh
+
+-------------------------------------------------------------------------------------------------- (While)
 
 
 -}
-       
