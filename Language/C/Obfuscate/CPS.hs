131a98a58fa88a1d80635fbe0e4101bf1d8affaf
Kenny Lu <luzhuomi@gmail.com>
2017-08-01 11:44:53 +0800

    updated

--- Language/C/Obfuscate/CPS.hs
+++ Language/C/Obfuscate/CPS.hs
@@ -70,20 +70,6 @@ data CPS = CPS { cps_decls :: [AST.CDeclaration N.NodeInfo]  -- ^ main function 
                           
                           
                           
-{-
-top level translation   p => P
-
-t => T    x => X     ti => Ti     xi => Xi     di => Di
-\bar{b} |- \bar{\Delta}    id, \bar{\Delta} |- \bar{b} => \bar{P}
-P1 = void f1 (void => void k) { B1 } 
--------------------------------------------------------------------------
-|- t x (\bar{t x}) {\bar{d};\bar{b}}  => 
-         T X (\bar{T X}) {\bar{D}; T rx; \bar{P}; f1(id); return rx; }
--}
-                          
-ssa2cps :: SSA -> CPS 
-ssa2cps = undefined
-
 
 class CPSize ssa cps where
   cps_trans :: ssa -> cps 
@@ -126,9 +112,26 @@ instance CPSize (AST.CDeclaration N.NodeInfo) (AST.CDeclaration N.NodeInfo) wher
     -- ; AST.CStaticAssert e lit nodeInfo -> undefined
     }
     
-{-                   
----------
+{- 
+translation  t => T
+
+-----------
+int => int
+
+-----------
+bool => bool
+
+t => t
+-------------
+t* => T*
+
 t => T
+------------
+t[] => T[]
+
+
+-------------
+void => void
 -}
                    
 instance CPSize (AST.CDeclarationSpecifier N.NodeInfo) (AST.CDeclarationSpecifier N.NodeInfo) where
@@ -149,5 +152,46 @@ data CDeclarationSpecifier a
 x => X
 -}
 
+instance CPSize (AST.CDeclarator N.NodeInfo) (AST.CDeclarator N.NodeInfo) where
+  cps_trans declr@(AST.CDeclr mb_ident derivedDecltrs mb_cstrLtr attrs nodeInfo) = declr 
 
 
+
+
+
+{-
+top level translation   p => P
+
+t => T    x => X     ti => Ti     xi => Xi     di => Di
+\bar{b} |- \bar{\Delta}    id, \bar{\Delta} |- \bar{b} => \bar{P}
+P1 = void f1 (void => void k) { B1 } 
+-------------------------------------------------------------------------
+|- t x (\bar{t x}) {\bar{d};\bar{b}}  => 
+         T X (\bar{T X}) {\bar{D}; T rx; \bar{P}; f1(id); return rx; }
+-}
+-- our target language C differs from the above specification.                          
+-- 1. the top function's type signature is not captured within SSA
+-- 2. \Delta is captured as the loop flag in LabaledBlock
+-- 3. there is no lambda expression, closure needs to be created as a context
+ssa2cps :: SSA -> CPS 
+ssa2cps (SSA scopedDecls labelledBlocks) = undefined
+
+
+mkContext :: [AST.CDeclaration N.NodeInfo] -> 
+
+
+{-
+
+typedef struct FibCtxt {
+  int x_0;
+  int (*cond2)(struct FibCtxt *);
+  void (*visitor3)(void (*k)(struct FibCtxt*), struct FibCtxt*);
+} fibctxt;
+
+
+
+CTranslUnit [CDeclExt (CDecl [CStorageSpec (CTypedef (NodeInfo ("test.c": line 3) (("test.c": line 3),7) (Name {nameId = 0}))),CTypeSpec (CSUType (CStruct CStructTag (Just (Ident "FibCtxt" 144044346 (NodeInfo ("test.c": line 3) (("test.c": line 3),7) (Name {nameId = 1})))) (Just [CDecl [CTypeSpec (CIntType (NodeInfo ("test.c": line 4) (("test.c": line 4),3) (Name {nameId = 3})))] [(Just (CDeclr (Just (Ident "x_0" 798712 (NodeInfo ("test.c": line 4) (("test.c": line 4),3) (Name {nameId = 2})))) [] Nothing [] (NodeInfo ("test.c": line 4) (("test.c": line 4),3) (Name {nameId = 4}))),Nothing,Nothing)] (NodeInfo ("test.c": line 4) (("test.c": line 4),3) (Name {nameId = 5})),CDecl [CTypeSpec (CIntType (NodeInfo ("test.c": line 5) (("test.c": line 5),3) (Name {nameId = 6})))] [(Just (CDeclr (Just (Ident "cond2" 211531797 (NodeInfo ("test.c": line 5) (("test.c": line 5),5) (Name {nameId = 7})))) [CPtrDeclr [] (NodeInfo ("test.c": line 5) (("test.c": line 5),5) (Name {nameId = 9})),CFunDeclr (Right ([CDecl [CTypeSpec (CSUType (CStruct CStructTag (Just (Ident "FibCtxt" 144044346 (NodeInfo ("test.c": line 5) (("test.c": line 5),7) (Name {nameId = 10})))) Nothing [] (NodeInfo ("test.c": line 5) (("test.c": line 5),7) (Name {nameId = 11}))) (NodeInfo ("test.c": line 5) (("test.c": line 5),7) (Name {nameId = 12})))] [(Just (CDeclr Nothing [CPtrDeclr [] (NodeInfo ("test.c": line 5) (("test.c": line 5),1) (Name {nameId = 13}))] Nothing [] (OnlyPos <no file> (<no file>,-1))),Nothing,Nothing)] (NodeInfo ("test.c": line 5) (("test.c": line 5),1) (Name {nameId = 14}))],False)) [] (NodeInfo ("test.c": line 5) (("test.c": line 5),1) (Name {nameId = 15}))] Nothing [] (NodeInfo ("test.c": line 5) (("test.c": line 5),5) (Name {nameId = 8}))),Nothing,Nothing)] (NodeInfo ("test.c": line 5) (("test.c": line 5),1) (Name {nameId = 16})),CDecl [CTypeSpec (CVoidType (NodeInfo ("test.c": line 6) (("test.c": line 6),4) (Name {nameId = 17})))] [(Just (CDeclr (Just (Ident "visitor3" 330935530 (NodeInfo ("test.c": line 6) (("test.c": line 6),8) (Name {nameId = 18})))) [CPtrDeclr [] (NodeInfo ("test.c": line 6) (("test.c": line 6),8) (Name {nameId = 20})),CFunDeclr (Right ([CDecl [CTypeSpec (CVoidType (NodeInfo ("test.c": line 6) (("test.c": line 6),4) (Name {nameId = 21})))] [(Just (CDeclr (Just (Ident "k" 107 (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 22})))) [CPtrDeclr [] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 24})),CFunDeclr (Right ([CDecl [CTypeSpec (CSUType (CStruct CStructTag (Just (Ident "FibCtxt" 144044346 (NodeInfo ("test.c": line 6) (("test.c": line 6),7) (Name {nameId = 25})))) Nothing [] (NodeInfo ("test.c": line 6) (("test.c": line 6),7) (Name {nameId = 26}))) (NodeInfo ("test.c": line 6) (("test.c": line 6),7) (Name {nameId = 27})))] [(Just (CDeclr Nothing [CPtrDeclr [] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 28}))] Nothing [] (OnlyPos <no file> (<no file>,-1))),Nothing,Nothing)] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 29}))],False)) [] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 30}))] Nothing [] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 23}))),Nothing,Nothing)] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 31})),CDecl [CTypeSpec (CSUType (CStruct CStructTag (Just (Ident "FibCtxt" 144044346 (NodeInfo ("test.c": line 6) (("test.c": line 6),7) (Name {nameId = 32})))) Nothing [] (NodeInfo ("test.c": line 6) (("test.c": line 6),7) (Name {nameId = 33}))) (NodeInfo ("test.c": line 6) (("test.c": line 6),7) (Name {nameId = 34})))] [(Just (CDeclr Nothing [CPtrDeclr [] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 35}))] Nothing [] (OnlyPos <no file> (<no file>,-1))),Nothing,Nothing)] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 36}))],False)) [] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 37}))] Nothing [] (NodeInfo ("test.c": line 6) (("test.c": line 6),8) (Name {nameId = 19}))),Nothing,Nothing)] (NodeInfo ("test.c": line 6) (("test.c": line 6),1) (Name {nameId = 38}))]) [] (NodeInfo ("test.c": line 3) (("test.c": line 7),1) (Name {nameId = 40}))) (NodeInfo ("test.c": line 3) (("test.c": line 7),1) (Name {nameId = 41})))] [(Just (CDeclr (Just (Ident "fibctxt" 211153242 (NodeInfo ("test.c": line 7) (("test.c": line 7),7) (Name {nameId = 39})))) [] Nothing [] (NodeInfo ("test.c": line 7) (("test.c": line 7),7) (Name {nameId = 42}))),Nothing,Nothing)] (NodeInfo ("test.c": line 3) (("test.c": line 7),1) (Name {nameId = 43})))] (NodeInfo ("test.c": line 3) (("test.c": line 7),1) (Name {nameId = 44}))
+
+-}
+
+cps_trans_lb = undefined
