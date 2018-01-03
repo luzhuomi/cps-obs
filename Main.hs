55707add63ac5c678408312d0f907962ff2a1e9f
Kenny Lu <luzhuomi@gmail.com>
2018-01-03 17:21:29 +0800

    added config.yaml

--- Main.hs
+++ Main.hs
@@ -12,41 +12,72 @@ import Language.C.Pretty (pretty)
 import Text.PrettyPrint.HughesPJ (render, text, (<+>), hsep)
 
 
+import Data.Text (Text, unpack, pack)
+import qualified Data.Yaml as Y
+import Data.Yaml (FromJSON(..), (.:))
+import Data.ByteString (ByteString, readFile)
+import Control.Applicative
+import Prelude hiding (readFile) -- Ensure Applicative is in scope and we have no warnings, before/after AMP.
+
+
 isMain fundef = case getFunName fundef of 
   { Just "main" -> True
   ; _ -> False
   }
 
+
+data Config =
+  Config {
+    gcc  :: Text
+  , whitelist  :: [Text]
+  } deriving (Eq, Show)
+
+
+instance FromJSON Config where
+  parseJSON (Y.Object v) =
+    Config <$>
+    v .:   (pack "gcc")       <*>
+    v .:   (pack "whitelist")
+  parseJSON _ = fail "Expected Object for Config value"
+
 -- gcc = "gcc" -- for linux
-gcc = "/usr/local/bin/gcc-7" -- for mac with homebrew
+-- gcc = "/usr/local/bin/gcc-7" -- for mac with homebrew
 
 main :: IO ()
 main = do 
   { let opts = []
   ; args <- getArgs
   ; let (src:dest:opts) = args
-  ; ast <- errorOnLeftM "Parse Error" $ parseCFile (newGCC gcc) Nothing opts src
-  ; case ast of 
-    { AST.CTranslUnit defs nodeInfo -> do 
-         { writeFile dest ""
-         ; mapM_ (\def -> case def of 
-                     { AST.CFDefExt fundef | not (isMain fundef) && not (isInlineFun fundef) -> 
-                          case runCFG fundef of
-                            { CFGOk (_, state) -> do 
-                                 { let ssa = buildSSA (cfg state) (formalArgs state)
-                                       cps = ssa2cps fundef ssa 
-                                 ; appendFile dest (prettyCPS cps) 
-                                 ; appendFile dest "\n"
-                                 }
-                            ; CFGError s       -> error s
-                            }
-                     ; other_def -> do 
-                          { appendFile dest (render $ pretty other_def)
-                          ; appendFile dest "\n"
-                          }
-                     }
-                 ) defs
-         }
-    ; _ -> error "not fundec"
+  ; configFile <- readFile "./config.yaml"
+  ; let mb_config :: Maybe Config 
+        mb_config = Y.decode configFile
+  ; case mb_config of 
+    { Nothing -> error "Error: config.yaml not valid"
+    ; Just config -> do 
+      { ast <- errorOnLeftM "Parse Error" $ parseCFile (newGCC (unpack $ gcc config)) Nothing opts src
+      ; case ast of 
+        { AST.CTranslUnit defs nodeInfo -> do 
+             { writeFile dest ""
+             ; mapM_ (\def -> case def of 
+                         { AST.CFDefExt fundef | not (isMain fundef) && not (isInlineFun fundef) -> 
+                              case runCFG fundef of
+                                { CFGOk (_, state) -> do 
+                                     { let ssa = buildSSA (cfg state) (formalArgs state)
+                                           cps = ssa2cps fundef ssa 
+                                     ; appendFile dest (prettyCPS cps) 
+                                     ; appendFile dest "\n"
+                                     }
+                                ; CFGError s       -> error s
+                                }
+                         ; other_def -> do 
+                              { appendFile dest (render $ pretty other_def)
+                              ; appendFile dest "\n"
+                              }
+                         }
+                     ) defs
+             }
+        ; _ -> error "Parse Failed" 
+        }
+      }
     }
   }
