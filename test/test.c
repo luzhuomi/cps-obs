9935c990d3aa3b3e6d6f16650b26367142d1279c
Kenny Lu <luzhuomi@gmail.com>
2018-08-12 22:52:04 +0800

    bumped to version 0.0.6

--- test/test.c
+++ test/test.c
@@ -1,44 +1,17 @@
-typedef struct _node {
-            short n_type;
-            char * n_str;
-            int n_lineno;
-            int n_col_offset;
+/*typedef struct S {
             int n_nchildren;
-            struct _node * n_child;
-        } node;
+        } ctxt;
 
-
-static char * ast_for_expr(const node * n)
-{
-  int i; // 0
- loop: // loop
-  switch (n->n_type) // 1
-    {
-    case 304:
-    case 302:
-      return n->n_str; // 2
-    case 305:
-    case 306:
-      if (n->n_nchildren == 1) // 3
-        { 
-	  n = &n->n_child[0]; // 4
-            goto loop;
-        }
-    }
-  return 0; // 5
+typedef void (*ctxt_arr_void)(ctxt*);
+*/
+/*
+void f () {
+  void (*f)(ctxt*) = rhs;
 }
+*/
 
-int main() {
-  node * p = (node *) malloc(sizeof(node));
-  node * c1 = (node *) malloc(sizeof(node));
-  //cnode * gc1 = (node *) malloc(sizeof(node));
-    
-  c1->n_str = "hello";
-  c1->n_type = 302;
-  p->n_str = "world";
-  p->n_type = 306;
-  p->n_nchildren = 1;
-  p->n_child = c1;
-  printf("%s",ast_for_expr(p));
-  return 0;
+void f(int x) {
+  if ((x == 1) || (x == 2)) {
+     return;
+  }
 }
