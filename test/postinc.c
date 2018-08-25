60adac2c07f1d71c23d9c926bf2f0c436ce258ec
Kenny Lu <luzhuomi@gmail.com>
2018-08-25 23:27:49 +0800

    added test/postinc.c as counter example, showing the need of adding a desugaring step for nested assignment or postinc

--- test/postinc.c
+++ test/postinc.c
@@ -2,8 +2,34 @@ int f() {
   int i = 1;
   int j = 0;
   
-  // j = (i++);
-  j = (i++);
+  // j = (i++)++ // gcc: expression is not assignable;
+  // (j++) = i++ // gcc: expression is not assignable;
+
+  // j = (i++) + (i+=1); // case 1
+  /*
+    the desugaring need to take place on the binary operator experession statement, maybe if statement too?
+   */
+
+  // step 1 desugar i++
+  // j = ((i=i+1)-1) + (i=i+1);
+
+  // step 2 move out the first nested assignment;
+  // i = i + 1;
+  // j = (i-1) + (i= i+1);
+
+  // step 3 move out the second nested assignment;
+
+  i = i + 1;
+  int i1 = i; // we need to save the state of i1 here, // no renaming is required if the expression is linear?!
+  i = i1 + 1;
+  int i2 = i;
+  j = (i1-1) + i2;
+
+
+  
+  
+  // (i++) + (j=i++); // case 2
+  
   i = i+1;
   return j;
 }
