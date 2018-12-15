eb755e2a5e053e85f11776d7c110b2d941205115
Kenny Lu <luzhuomi@gmail.com>
2018-12-15 13:50:51 +0800

    updated

--- test/test_python.sh
+++ test/test_python.sh
@@ -1,2 +1,2 @@
-~/git/cpp-obs/dist/build/cpp-obs/cpp-obs ast_orig.c ast.c -I ~/Downloads/Python-2.7.14/ -I ~/Downloads/Python-2.7.14/Include/ -I .
+~/git/cps-obs/dist/build/cps-obs/cps-obs ast_orig.c ast.c -I ~/Downloads/Python-2.7.14/ -I ~/Downloads/Python-2.7.14/Include/ -I .
 
